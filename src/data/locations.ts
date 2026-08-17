0c0e003c826aaf3453038c65f7e829e33251164f
apothekenluft <thomas@apothekenluft.de>
2026-08-18 00:13:57 +0200

    Update locations.ts
    Adressänderung wegen Umuzg

--- src/data/locations.ts
+++ src/data/locations.ts
@@ -3,17 +3,17 @@ export const locations = [
     logo: '/images/logo-postapo.webp',
     bild: '/images/aussenansicht-postapo.webp',
     name: 'Post-Apotheke Neckarhausen',
-    adresse: 'Theresienstr. 2, 68535 Edingen-Neckarhausen',
+    adresse: 'Hauptstr. 382, 68535 Edingen-Neckarhausen',
     tel: '+49 6203 12577',
     telLink: '+49620312577',
     fax: '+49 6203 17188',
     mailUser: 'info',
     mailDomain: 'apotheke-neckarhausen.de',
     web: 'https://www.apotheke-neckarhausen.de',
-    openstreetmapUrl: 'https://www.openstreetmap.org/search?query=Theresienstr.%202,%2068535%20Edingen-Neckarhausen',
-    // Location: 49.4676174, 8.5992084
-    lat: 49.4676174,
-    lon: 8.5992084,
+    openstreetmapUrl: 'https://www.openstreetmap.org/search?query=Hauptstr.%20382,%2068535%20Edingen-Neckarhausen',
+    // Location: 49.4652907, 8.6015661
+    lat: 49.4652907,
+    lon: 8.6015661,
     zoom: 15,
     appstore: 'https://apps.apple.com/us/app/post-apotheke-neckarhausen/id6475203802',
     playstore: 'https://play.google.com/store/apps/details?id=com.apo2u.apoinhv.app'
